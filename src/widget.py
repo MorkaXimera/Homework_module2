0acc02e52d42c9694b6719d5016add21ea029bf2
Alyona Goncharova <morkaximera@gmail,com>
2025-07-24 00:27:09 +0300

    widget.py rechecked by flake8, mypy

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,7 @@
 from mask import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(discription: str):
+def mask_account_card(discription: str) -> str:
     """Функция возвращает маску номера счета или карты"""
     insert_data = discription.split(" ")
     if insert_data[0] == "Счет":
@@ -12,11 +12,10 @@ def mask_account_card(discription: str):
     return masked_account
 
 
-def get_date(date_str: str):
-    '''Функция возвращает дату в формате ДД.ММ.ГГГГ'''
+def get_date(date_str: str) -> str:
+    """Функция возвращает дату в формате ДД.ММ.ГГГГ"""
     only_date_str = date_str[:10]
-    only_date_list = only_date_str.split('-')
+    only_date_list = only_date_str.split("-")
     only_date_list.reverse()
-    only_date = '.'.join(only_date_list)
-    return (only_date)
-
+    only_date = ".".join(only_date_list)
+    return only_date
