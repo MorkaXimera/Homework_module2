7d8bb15ce3e75e7992380705c4fd4579043b7adc
Alyona Goncharova <morkaximera@gmail,com>
2025-08-17 20:54:47 +0300

    readme updated, checked by flake8, mypy

--- src/generators.py
+++ src/generators.py
@@ -1,22 +1,30 @@
-def filter_by_currency(transactions, type='USD'):
+def filter_by_currency(transactions: list, type="USD") -> list:
+    """
+    Функция возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной
+    """
     for transaction in transactions:
-        if transaction['operationAmount']['currency']['name'] == type:
-          yield transaction
+        if transaction["operationAmount"]["currency"]["name"] == type:
+            yield transaction
 
 
-def transaction_descriptions(transactions):
+def transaction_descriptions(transactions: list) -> list:
+    """
+    Функция возвращает описание каждой операции
+    """
     for transaction in transactions:
-          yield transaction['description']
+        yield transaction["description"]
 
 
-def card_number_generator(start=1, stop=10000000000000000):
-    zero_number = '0000000000000000'
+def card_number_generator(start=1: int, stop=10000000000000000: int) -> str:
+    """
+    Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX
+    """
+    zero_number = "0000000000000000"
     new_card_list = list(zero_number)
     for i in range(start, stop):
-      new_chars = list(str(i))
-      new_card_list = new_card_list[:-len(new_chars)]
-      new_card_list = new_card_list + new_chars
-      new_card_number = ''.join(new_card_list)
-      new_card_number_spaced = ' '.join(new_card_number[i:i+4] for i in range(0, len(new_card_number), 4))
-      yield new_card_number_spaced
-
+        new_chars = list(str(i))
+        new_card_list = new_card_list[: -len(new_chars)]
+        new_card_list = new_card_list + new_chars
+        new_card_number = "".join(new_card_list)
+        new_card_number_spaced = " ".join(new_card_number[i: i + 4] for i in range(0, len(new_card_number), 4))
+        yield new_card_number_spaced
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,21 +1,19 @@
-import pytest
-
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency_usd(transactions_coll: list, filtred_by_currency_usd: list) -> list:
-   assert list(filter_by_currency(transactions_coll, 'USD')) == filtred_by_currency_usd
+    assert list(filter_by_currency(transactions_coll, "USD")) == filtred_by_currency_usd
 
 
 def test_filter_by_currency_notype(transactions_coll: list, filtred_by_currency_usd: list) -> list:
-   assert list(filter_by_currency(transactions_coll)) == filtred_by_currency_usd
+    assert list(filter_by_currency(transactions_coll)) == filtred_by_currency_usd
 
 
 def test_filter_by_currency_no_operations(transactions_coll: list) -> list:
-   assert list(filter_by_currency(transactions_coll, 'EUR')) == []
+    assert list(filter_by_currency(transactions_coll, "EUR")) == []
 
 
-def test_filter_by_currency_no_list():
+def test_filter_by_currency_no_list() -> list:
     transactions = []
     assert list(filter_by_currency(transactions)) == []
 
@@ -24,8 +22,8 @@ def test_transaction_descriptions(transactions_coll: list, description_call: lis
     assert list(transaction_descriptions(transactions_coll)) == description_call
 
 
-def test_transaction_descriptions_usd (filtred_by_currency_usd: list, description_call_usd: list) -> list:
-    assert list (transaction_descriptions(filtred_by_currency_usd)) == description_call_usd
+def test_transaction_descriptions_usd(filtred_by_currency_usd: list, description_call_usd: list) -> list:
+    assert list(transaction_descriptions(filtred_by_currency_usd)) == description_call_usd
 
 
 def test_transaction_descriptions_no_list():
@@ -33,13 +31,14 @@ def test_transaction_descriptions_no_list():
     assert list(transaction_descriptions(transactions)) == []
 
 
-def test_card_number_generator():
+def test_card_number_generator() ->str:
     generator = card_number_generator()
-    assert next(generator) == '0000 0000 0000 0001'
-    assert next(generator) == '0000 0000 0000 0002'
-    assert next(generator) == '0000 0000 0000 0003'
-    assert next(generator) == '0000 0000 0000 0004'
+    assert next(generator) == "0000 0000 0000 0001"
+    assert next(generator) == "0000 0000 0000 0002"
+    assert next(generator) == "0000 0000 0000 0003"
+    assert next(generator) == "0000 0000 0000 0004"
 
-def test_card_number_generator_max():
+
+def test_card_number_generator_max() -> str:
     generator = card_number_generator(9999999999999999)
-    assert next(generator) == '9999 9999 9999 9999'
+    assert next(generator) == "9999 9999 9999 9999"
