bdac2730f2d64b652ca302a81712aa7ce696a8e0
Alyona Goncharova <morkaximera@gmail,com>
2025-09-10 01:19:49 +0300

    .env and .env.example created

--- src/external_api.py
+++ src/external_api.py
@@ -1,5 +1,7 @@
 import requests
 from utils import json_read
+import os
+from dotenv import load_dotenv
 
 def transaction_amount():
     path = r'C:\Users\morka\PycharmProjects\Homework_2\data\operations.json'
@@ -10,9 +12,10 @@ def transaction_amount():
                 print(dict['operationAmount']['amount'])
             elif dict['operationAmount']['currency']['code'] == 'EUR' or 'USD':
                 url = "https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}"
+                api_key = os.getenv('API_KEY')
                 payload = {f'{{"to":"RUB", "from": {dict["operationAmount"]["currency"]["code"]}, "amount":{dict["operationAmount"]["amount"]}}}'}
                 headers = {
-                    "apikey": "k5gXEs1Gn3ImdFhp43TADIPICl2J9sNH"
+                    "apikey": f'{api_key}'
                 }
                 response = requests.request("GET", url, headers=headers, data = payload)
                 status_code = response.status_code
