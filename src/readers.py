885ffc789e8f17f7b97ca40426fab70e55bc007b
Alyona Goncharova <morkaximera@gmail,com>
2025-09-18 03:50:59 +0300

    csv reader added

---
+++ src/readers.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def csv_reader(csv_file_path) -> list:
+    '''
+    Функция позволяет прочесть транзакции из файла в формате .csv и возвращает список словарей
+    '''
+    transaction_df = pd.read_csv(csv_file_path, sep=';')
+    transaction_list = transaction_df.to_dict(orient='records')
+    return transaction_list
+
+
+def xlsx_reader(excel_file_path) -> list:
+    '''
+    Функция позволяет прочесть транзакции из файла в формате .xlsx и возвращает список словарей
+    '''
