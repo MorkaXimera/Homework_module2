89051a14210baedc570c3c4f6b7b59c176a6d68c
Alyona Goncharova <morkaximera@gmail,com>
2025-09-24 20:53:10 +0300

    tests for xlsx and cvs reader added

--- src/readers.py
+++ src/readers.py
@@ -1,19 +1,28 @@
 import pandas as pd
 
 
-def csv_reader(csv_file_path) -> list:
+def csv_reader(csv_file_path = 'no.csv') -> list:
     '''
     Функция позволяет прочесть транзакции из файла в формате .csv и возвращает список словарей
     '''
-    transaction_df = pd.read_csv(csv_file_path, sep=';')
-    transaction_list = transaction_df.to_dict(orient='records')
-    return transaction_list
+    try:
+        transaction_df = pd.read_csv(csv_file_path, sep=';')
+        transaction_list = transaction_df.to_dict(orient='records')
+        return transaction_list
+    except FileNotFoundError:
+        return []
 
 
-def xlsx_reader(excel_file_path) -> list:
+def xlsx_reader(excel_file_path = 'no.xlsx') -> list:
     '''
     Функция позволяет прочесть транзакции из файла в формате .xlsx и возвращает список словарей
     '''
-    transaction_df = pd.read_excel(excel_file_path)
-    transaction_list = transaction_df.to_dict(orient='records')
-    return transaction_list
+    try:
+        transaction_df = pd.read_excel(excel_file_path)
+        transaction_list = transaction_df.to_dict(orient='records')
+        return transaction_list
+    except FileNotFoundError:
+        return []
+
+
+print(xlsx_reader('C:/Users/morka/PycharmProjects/files_for_homework_2/transactions_excel.xlsx'))
---
+++ tests/test_readers.py
@@ -0,0 +1,66 @@
+from src.readers import xlsx_reader, csv_reader
+import pytest
+from unittest.mock import patch, Mock
+import pandas as pd
+
+
+def test_csv_reader_no_file():
+    assert csv_reader() == []
+
+
+def test_csv_reader_invalid_file():
+    assert csv_reader("C:/Users/morka/PycharmProjects/Homework_2/tests/nofile.csv") == []
+
+
+@patch('pandas.read_csv')
+def test_csv_reader(mock_read_csv):
+    mock_data = ([{'id': 4137938.0,
+                                 'state': 'EXECUTED',
+                                 'date': '2023-01-04T13:13:34Z',
+                                 'amount': 15560.0,
+                                 'currency_name': 'Real',
+                                 'currency_code': 'BRL',
+                                 'from': 'Discover 5243103907617572',
+                                 'to': 'Счет 38164279390569873521',
+                                 'description': 'Открытие вклада'}])
+    mock_df = pd.DataFrame(mock_data)
+    mock_read_csv.return_value = mock_df
+    assert csv_reader("my_path.csv") == [{'id': 4137938.0,
+                                 'state': 'EXECUTED',
+                                 'date': '2023-01-04T13:13:34Z',
+                                 'amount': 15560.0,
+                                 'currency_name': 'Real',
+                                 'currency_code': 'BRL',
+                                 'from': 'Discover 5243103907617572',
+                                 'to': 'Счет 38164279390569873521',
+                                 'description': 'Открытие вклада'}]
+
+
+def test_xlsx_reader_no_file():
+    assert xlsx_reader() == []
+
+def test_xlsx_reader_invalid_file():
+    assert csv_reader("C:/Users/morka/PycharmProjects/Homework_2/tests/nofile.csv") == []
+
+@patch('pandas.read_excel')
+def test_xlsx_reader(mock_read_excel):
+    mock_data = ([{'id': 4137938.0,
+                       'state': 'EXECUTED',
+                       'date': '2023-01-04T13:13:34Z',
+                       'amount': 15560.0,
+                       'currency_name': 'Real',
+                       'currency_code': 'BRL',
+                       'from': 'Discover 5243103907617572',
+                       'to': 'Счет 38164279390569873521',
+                       'description': 'Открытие вклада'}])
+    mock_df = pd.DataFrame(mock_data)
+    mock_read_excel.return_value = mock_df
+    assert xlsx_reader("my_path.excel") == [{'id': 4137938.0,
+                                                 'state': 'EXECUTED',
+                                                 'date': '2023-01-04T13:13:34Z',
+                                                 'amount': 15560.0,
+                                                 'currency_name': 'Real',
+                                                 'currency_code': 'BRL',
+                                                 'from': 'Discover 5243103907617572',
+                                                 'to': 'Счет 38164279390569873521',
+                                                 'description': 'Открытие вклада'}]
