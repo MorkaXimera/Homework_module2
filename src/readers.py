2ff14000085b234e9554cbd4a1c522f5514340e4
Alyona Goncharova <morkaximera@gmail,com>
2025-09-24 21:17:50 +0300

    black, isort, flake8, mypy

--- src/readers.py
+++ src/readers.py
@@ -1,30 +1,29 @@
 import pandas as pd
 
+from typing import Any
 
-def csv_reader(csv_file_path = 'no.csv') -> list:
-    '''
+
+def csv_reader(csv_file_path : str ="no.csv") -> list[Any]:
+    """
     Функция позволяет прочесть транзакции из файла в формате .csv и возвращает список словарей
-    '''
+    """
     correct_file_path = str(csv_file_path)
     try:
-        transaction_df = pd.read_csv(correct_file_path, sep=';')
-        transaction_list = transaction_df.to_dict(orient='records')
+        transaction_df = pd.read_csv(correct_file_path, sep=";")
+        transaction_list = transaction_df.to_dict(orient="records")
         return transaction_list
     except FileNotFoundError:
         return []
 
 
-def xlsx_reader(excel_file_path = 'no.xlsx') -> list:
-    '''
+def xlsx_reader(excel_file_path : str ="no.xlsx") -> list[Any]:
+    """
     Функция позволяет прочесть транзакции из файла в формате .xlsx и возвращает список словарей
-    '''
+    """
     correct_file_path = str(excel_file_path)
     try:
         transaction_df = pd.read_excel(correct_file_path)
-        transaction_list = transaction_df.to_dict(orient='records')
+        transaction_list = transaction_df.to_dict(orient="records")
         return transaction_list
     except FileNotFoundError:
         return []
-
-
-print(xlsx_reader(123))
